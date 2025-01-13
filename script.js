b76a61a631a3f214b17d879124543019fcd5e219
fidelisobioma <mbamfidelisobioma1@gmail.com>
2025-01-13 01:40:50 +0100

    Made the following changes: * I restructured the HTML file to have a better layout * I made changes to the CSS files to match with the HTML file * Commented the script file since I'm only working on the UI for now

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
-let display = document.querySelector(".display");
-const operands = document.querySelectorAll(".btn");
-let allSymbols = ["+", "-", "*", "/", "=", "AC", "del"];
-let firstValue = "";
-let secondValue = "";
-let symbol = "";
+let Values = document.querySelector(".values");
+let calculate = document.querySelector(".result");
+let buttons = document.querySelector(".buttons");
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
 let result = "";
+let symbol = "- + * /";
+buttons.addEventListener("click", (btn) => {
+  let button = btn.target.textContent;
+  // operator = "";
+  let onlysymbols = symbol.includes(button);
+  // console.log(onlysymbols);
+  if (onlysymbols) {
+    symbol = button;
+    console.log("first" + symbol);
+  } else if (symbol !== button) {
+    firstNumber = button;
+    console.log("first" + firstNumber);
+  } else if (symbol) {
+    secondNumber = button;
+    console.log("second" + secondNumber);
+  }
 
-// console.log(result);
-operands.forEach((btn) => {
-  btn.addEventListener("click", (btn) => {
-    let target = btn.target.textContent;
-    if (target !== "AC" && target !== "del") {
-      display.textContent += target;
-    }
-    let isSymbol = allSymbols.includes(target);
-    if (firstValue && isSymbol) {
-      symbol = target;
-    } else if (!symbol) {
-      firstValue += target;
-    } else if (symbol) {
-      secondValue += target;
-    }
+  if (secondNumber) {
+    result = firstNumber + secondNumber;
+    console.log(result);
+  }
 
-    if (symbol === "+") result = +firstValue + +secondValue;
-    if (symbol === "*") result = +firstValue * +secondValue;
-    if (symbol === "/") result = +firstValue / +secondValue;
-    if (symbol === "-") result = +firstValue - +secondValue;
-
-    if (symbol == "=") {
-      display.textContent = result;
-      firstValue = +display.textContent;
-      secondValue = "";
-    }
-    if (symbol === "AC") {
-      location.reload();
-    }
-  });
+  // console.log(button);
 });
